15ac515a9b6f6656ad8b3f8c014332d3718f7d63
Jaqueline <jaqueline1@ice.ufjf.br>
2022-06-13 21:07:07 -0300

    adiciona funcao para ler arquivo

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,48 @@
 
 using namespace std;
 
+// Ler arquivo.
+vector<string> leArquivo(string arquivo)
+{
+    vector<string> aux;
+
+    // Abre arquivo.
+    ifstream input_file;
+    input_file.open("../input/" + arquivo, ios::in);
+
+    // Verifica se arquivo foi aberto.
+    if (!input_file.is_open())
+    {
+        cout << "[ERROR] Nao foi possivel abrir arquivo." << endl;
+        exit(2);
+    }
+
+    // Separa cada linha do arquivo em uma string.
+    string line;
+    while (getline(input_file, line))
+    {
+        aux.push_back(line);
+    }
+
+    // Verifica se arquivo esta vazio.
+    if (aux.empty())
+    {
+        cout << "[WARNING] Nunhuma string foi carregada do arquivo." << endl;
+    }
+
+    // Imprimindo strings para teste.
+    // cout << endl;
+    // for (vector<string>::iterator it = aux.begin(); it != aux.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+    cout << "[INFO] Arquivo carregado." << endl;
+
+    // Fecha arquivo.
+    input_file.close();
+    return aux;
+}
+
 void menu()
 {
     cout << endl;
@@ -72,12 +114,12 @@ int main()
         switch (aux[0].at(1))
         {
         case 'd':
-            //input_string = leArquivo(aux[1]);
+            input_string = leArquivo(aux[1]);
             cout << "[WARNING] Funcionalidade de dividir string nao implementada." << endl;
             // FAZER ----> Dividir a string em tags.
             break;
         case 'c':
-            //input_tags = leArquivo(aux[1]);
+            input_tags = leArquivo(aux[1]);
             //tagsValidas = divideTag(input_tags, tagsValidas);
             break;
         case 'o':
